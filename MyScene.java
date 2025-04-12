d928eeea092d86103a041649d5fcaec46c01668c
chloekp <cpun0004@mymail.lausd.net>
2025-04-11 20:38:08 -0700

    Update MyScene.java

--- MyScene.java
+++ MyScene.java
@@ -1,1 +1,24 @@
+import org.code.theater.*;
+import org.code.media.*;
 
+public class MyScene extends Scene {
+  private String playerInput;
+  private String ballInput;
+
+  public MyScene(String playerInput, String ballInput) {
+    this.playerInput = playerInput;
+    this.ballInput = ballInput;
+  }
+
+  // Draws the scene using player and ball objects
+  public void drawScene() {
+    GameObject player = new Player(playerInput);
+    GameObject ball = new Ball(ballInput);
+
+    player.display();
+    ball.display();
+
+    drawText("You chose to be a " + playerInput + " with a " + ballInput + " ball!", 20, 20);
+  }
+
+}
